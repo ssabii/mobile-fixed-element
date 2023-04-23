702b9ab8a34877f75f510b05a0eb0dcd82ea6d07
ssabii <fhy1930@gmail.com>
2023-04-23 09:58:21 +0900

    wip: 인풋 클릭 시 키보드 상단 엘리먼트 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Container, Input, Button } from '@chakra-ui/react'
+import styled from '@emotion/styled';
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [height, setHeight] = useState(0);
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  }
+
+  const handleBlur = () => {
+    setFocused(false);
+  }
+
+  const handleResize = () => {
+    const innerHeight = window.innerHeight;
+    const visualViewportHeight = window.visualViewport?.height || 0;
+    const keyboardHeight = innerHeight - visualViewportHeight;
+    setHeight(keyboardHeight);
+  }
+
+  const handleScroll = () => {
+    const rect = document.body.getBoundingClientRect();
+    const max = window.innerHeight;
+    const value = max - (window.visualViewport?.height || 0) + rect.y;
+    setHeight(value);
+  }
+
+  useEffect(() => {
+    window.visualViewport?.addEventListener('scroll', handleScroll);
+    window.visualViewport?.addEventListener('resize', handleResize);
+
+    return () => {
+      window.visualViewport?.removeEventListener('scroll', handleScroll);
+      window.visualViewport?.removeEventListener('resize', handleResize);
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container
+      height="1000px"
+      onScroll={handleScroll}
+    >
+      {height}
+      <Input
+        ref={inputRef}
+        mb="8px"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      <Wrapper style={{
+        position: focused ? 'fixed' : 'static',
+        bottom: focused ? height : 0,
+        left: 0,
+        right: 0,
+      }}>
+        <Button
+          width="100%"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          클릭
+        </Button>
+      </Wrapper>
+    </Container >
   );
 }
+
+const Wrapper = styled.div``;
 
 export default App;
